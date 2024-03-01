65e413c9b705b2981123df1d5c679e60c67006a5
ADevph <das.atanu42914@gmail.com>
2024-03-02 02:36:24 +0600

    project initial set up complete

---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Userlist from './components/Userlist';
+import UserDetailsPage from './components/UserDetailsPage';
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './App.css';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div><Userlist /></div>,
+  },
+  {
+    path: "/user/:userId",
+    element: <UserDetailsPage />
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
