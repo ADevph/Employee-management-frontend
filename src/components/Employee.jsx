1a3987c0223cae0e5a1a630398d6b77d72bc2df4
ADevph <das.atanu42914@gmail.com>
2024-03-02 11:26:27 +0600

    Responsivness fixed

--- src/components/Employee.jsx
+++ src/components/Employee.jsx
@@ -12,11 +12,11 @@ const Employee = () => {
     <div className="container mx-auto mt-8">
       <AddUser addUser={addUser} />
       <div className="mt-8">
-        <h2 className="text-2xl font-semibold mb-4">Employees</h2>
+        <h2 className="text-2xl font-semibold mb-4 text-center">Employees</h2>
         {employees.length === 0 ? (
           <p> No employee data. Add now! </p>
         ) : (
-          <table className="w-full border-collapse">
+<table className="w-full sm:w-400 md:w-800 text-center items-center justify-center sm:ml-0 sm:mx-auto border-collapse">
             <thead>
               <tr>
                 <th className="border px-4 py-2">Full Name</th>
@@ -27,7 +27,7 @@ const Employee = () => {
               {employees && employees.map((employee, index) => (
                 <tr key={index}>
                   <td className="border px-4 py-2">{`${employee.firstName} ${employee.lastName}`}</td>
-                  <td className="border px-4 py-2">
+                  <td className="border px-4 py-2 text-center">
                     <button className="mr-2 bg-blue-500 text-white px-2 py-1 rounded-md">Details</button>
                     <button className="mr-2 bg-yellow-500 text-white px-2 py-1 rounded-md">Block</button>
                     <button className="bg-red-500 text-white px-2 py-1 rounded-md">Delete</button>
